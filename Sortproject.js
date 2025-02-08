10ffcc33fa70c6bf33a89804b4fa71284e04fa9b
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2025-02-08 22:08:31 +0530

    Add files via upload

---
+++ Sortproject.js
@@ -0,0 +1,10 @@
+var button = document.querySelector('button');
+
+button.onclick = function() {
+// Add your solution here
+var p = document.createElement('p');
+    p.innerHTML = "I ❤️love 😘💋 you !";
+
+    var content = document.querySelector('body');
+    content.appendChild(p)
+}
