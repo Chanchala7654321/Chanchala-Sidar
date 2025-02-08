24ec5a8d258bafdb3d5b1db13ad1f9134e496c78
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2025-02-08 22:11:02 +0530

    Update Sortproject.js

--- Sortproject.js
+++ Sortproject.js
@@ -3,7 +3,7 @@ var button = document.querySelector('button');
 button.onclick = function() {
 // Add your solution here
 var p = document.createElement('p');
-    p.innerHTML = "I ❤️love 😘💋 you !";
+    p.innerHTML = "I ❤️love 😘💋 you ji !";
 
     var content = document.querySelector('body');
     content.appendChild(p)
